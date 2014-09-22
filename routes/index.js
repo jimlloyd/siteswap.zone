c326b9d5ed9a870a324ff45b9066d8b74e0699b8
Jim Lloyd <jim.lloyd@gmail.com>
2014-09-21 17:07:47 -0700

    Fix problems with 404 error when ss not found.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var lookup = require('../lib/lookup.js');
 var _ = require('lodash');
+var dlog = require('debug')('index');
+var express = require('express');
 var fs = require('fs');
+var lookup = require('../lib/lookup.js');
+var router = express.Router();
+var util = require('util');
 
 /* GET favicon. */
 router.get('/favicon.ico', function(req, res) {
@@ -19,35 +21,47 @@ router.get('/', function(req, res) {
     });
 });
 
+function notFound(res, ss) {
+  res.status(404).send(util.format('Siteswap %s not in index.', ss));
+}
+
 /* Redirect siteswap to imgur. */
 router.get('/:ss', function(req, res) {
   var ss = req.params.ss;
-  console.log('Looking up siteswap for title:', ss);
+  dlog('Looking up siteswap for title:', ss);
   lookup.lookupByTitle(ss)
     .then(function (match) {
-      console.log('Lookup for ', ss, ' returned:', match);
+      dlog('Lookup for %s returned: %j', ss, match);
       if (_.isObject(match))
           res.redirect(match.link);
       else {
-          res.status(404);
+          notFound(res, ss);
       }
-    });
+    })
+    .catch(function (err) {
+      notFound(res, ss);
+    })
+    .done();
 });
 
 /* Display siteswap GIF in a local page. */
 router.get('/z/:ss', function(req, res) {
   var ss = req.params.ss;
-  console.log('Looking up siteswap for title:', ss);
+  dlog('Looking up siteswap for title: %s', ss);
   lookup.lookupByTitle(ss)
     .then(function (match) {
-      console.log('Lookup for ', ss, ' returned:', match);
+      dlog('Lookup for %s returned: %j', ss, match);
       var imgur = match.link.substr(0, match.link.length-4);
       if (_.isObject(match))
         res.render('onesiteswap', { title: match.title, img: match.link, imgur: imgur });
       else {
-          res.status(404);
+        notFound(res, ss);
       }
-    });
+    })
+    .catch(function (err) {
+      notFound(res, ss);
+    })
+    .done();
 });
 
 module.exports = router;
